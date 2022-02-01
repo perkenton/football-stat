e977968e919c3e37900196d0a9549db590b1f6a7
Alexander Vasiliev <selado@ya.ru>
2022-02-02 03:39:48 +0500

    Создана переменная с базовыми данными для запросов к API

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,8 @@
+import axios from 'axios';
+
 export const apiUrl = process.env.REACT_APP_API_URL;
 export const apiToken = process.env.REACT_APP_API_TOKEN;
+export const instance = axios.create({
+  baseURL: apiUrl,
+  headers: { 'X-Auth-Token': apiToken! }
+});

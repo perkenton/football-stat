a6e1bde5db6bfc0684acf452075a422c29a87a63
Alexander Vasiliev <selado@ya.ru>
2022-02-02 03:45:58 +0500

    Прописан Route и компоненты Header и CompetitionsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import styles from './App.module.scss';
+import Header from './components/Header/Header';
+import CompetitionsPage from './components/Competitions/components/CompetitionsPage';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Header />
+      <main className={ styles.main }>
+        <Routes>
+          <Route path='/' element={ <CompetitionsPage /> } />
+        </Routes>
+      </main>
+    </HashRouter>
   );
 }
 

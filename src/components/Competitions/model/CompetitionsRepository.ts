1e19b70a9f9fe67bd22d883328c7451902559ada
Alexander Vasiliev <selado@ya.ru>
2022-02-02 03:41:50 +0500

    Настроен класс для получения данных соревнований

---
+++ src/components/Competitions/model/CompetitionsRepository.ts
@@ -0,0 +1,24 @@
+import { instance } from '../../../common/constants';
+import { Competition, Plan } from '../../../common/model';
+
+export interface CompetitionsRepository {
+  getCompetitions(): Promise<Competition[]>;
+}
+
+export class CompetitionsRepositoryImpl implements CompetitionsRepository {
+
+  async getCompetitions(): Promise<Competition[]> {
+    const plan = Plan.TIER_ONE;
+
+    const response = await instance
+      .get(`competitions?areas=2077&plan=${plan}`)
+      .then((res) => {
+        return res.data
+      })
+      .catch((error) => {
+        console.log('getCompetitions error', error);
+      });
+
+    return response;
+  }
+}

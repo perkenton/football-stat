dcbacc06547027fc1d1f321f99fdafcf3bacc51c
Alexander Vasiliev <selado@ya.ru>
2022-02-02 03:41:09 +0500

    Добавлены типы данных соревнований

---
+++ src/common/model.ts
@@ -0,0 +1,32 @@
+
+export type Area = {
+  id: number,
+  name: string
+  countryCode: string,
+  ensignUrl :string,
+}
+
+export enum Plan {
+  TIER_ONE = 'TIER_ONE',
+}
+
+export type Season = {
+  id: number,
+  startDate: Date,
+  endDate: Date,
+  currentMatchday: number,
+  winner?: string,
+}
+
+export type Competition = {
+  id: number,
+  area: Area,
+  name: string,
+  code?: string,
+  emblemUrl?: string,
+  plan: Plan,
+  currentSeason: Season,
+  seasons: Season[],
+  numberOfAvailableSeasons: number,
+  lastUpdated: Date,
+}

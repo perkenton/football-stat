6b05acd5a3e19cbc3b86d1cbf3e192674311d3b6
Alexander Vasiliev <selado@ya.ru>
2022-02-03 02:31:22 +0500

    Настроено отображение турниров

--- src/components/Competitions/components/Competitions.tsx
+++ src/components/Competitions/components/Competitions.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Competitions.module.scss';
 import { CompetitionsRepository } from '../model/CompetitionsRepository';
 import { Competition } from '../../../common/model';
@@ -16,9 +16,28 @@ function Competitions(props: { competitionsRepository: CompetitionsRepository })
 
 
   return (
-    <div>
-        competitions data
-    </div>
+    <>
+      <h1 className={ styles.title }>Турниры</h1>
+      <ul className={ styles.competitionsList }>
+        {
+          competitions.map((item: Competition) => {
+            return (
+              <a href={ `#${item.id}` } className={ styles.cardLink } key={ item.id }>
+                <li className={ styles.card }>
+                  <figure className={ styles.cardHeader }>
+                    <img src={ item.area.ensignUrl || item.emblemUrl } alt='Логотип турнира' className={ styles.cardImg } />
+                  </figure>
+                  <figcaption className={ styles.cardBody }>
+                    <p className={ styles.cardTitle }>{ item.name }</p>
+                    <p className={ styles.cardSubtitle }>{ item.area.name }</p>
+                  </figcaption>
+                </li>
+              </a>
+            )
+          })
+        }
+      </ul>
+    </>
   );
 }
 

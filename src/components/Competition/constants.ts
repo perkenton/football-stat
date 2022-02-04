be961960975f00ef68fc053556b373db89f193e1
Alexander Vasiliev <selado@ya.ru>
2022-02-05 04:29:18 +0500

    Добавлен объект со значениями статуса матча для отображения в таблице

---
+++ src/components/Competition/constants.ts
@@ -0,0 +1,10 @@
+export const MATCH_STATUS_MAP: any = {
+  SCHEDULED: 'По расписанию',
+  LIVE: 'Идёт сейчас',
+  IN_PLAY: 'Идёт сейчас',
+  PAUSED: 'Перерыв',
+  FINISHED: 'Закончен',
+  POSTPONED: 'Перенесен',
+  SUSPENDED: 'Приостановлен',
+  CANCELED: 'Отменен',
+}

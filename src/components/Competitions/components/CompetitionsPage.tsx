fd003511053991661e8f18b9f41cee1d659dbf0d
Alexander Vasiliev <selado@ya.ru>
2022-02-06 04:58:05 +0500

    В презентер передан репозиторий, исправлен передаваемый пропс

--- src/components/Competitions/components/CompetitionsPage.tsx
+++ src/components/Competitions/components/CompetitionsPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { CompetitionsRepositoryImpl, CompetitionsRepository } from '../model/CompetitionsRepository';
+import { CompetitionsPresenter, CompetitionsPresenterImpl } from '../model/CompetitionsPresenter';
 import Competitions from './Competitions';
 
 
 function CompetitionsPage() {
   const competitionsRepository: CompetitionsRepository = new CompetitionsRepositoryImpl();
+  const competitionsPresenter: CompetitionsPresenter = new CompetitionsPresenterImpl(competitionsRepository);
 
-  return <Competitions competitionsRepository={ competitionsRepository } />
+  return <Competitions competitionsPresenter={ competitionsPresenter } />
 }
 
 export default CompetitionsPage;

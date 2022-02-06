b05ef53dd683cb12114afeabd2f00509b475988e
Alexander Vasiliev <selado@ya.ru>
2022-02-07 03:41:17 +0500

    Исправлена обертка компонента для работы через презентер

--- src/components/Competition/components/CompetitionPage.tsx
+++ src/components/Competition/components/CompetitionPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { CompetitionRepositoryImpl, CompetitionRepository } from '../model/CompetitionRepository';
+import { CompetitionPresenterImpl, CompetitionPresenter } from '../model/CompetitionPresenter';
 import Competition from './Competition';
 
 
 function CompetitionPage() {
   const competitionRepository: CompetitionRepository = new CompetitionRepositoryImpl();
+  const competitionPresenter: CompetitionPresenter = new CompetitionPresenterImpl(competitionRepository);
 
-  return <Competition competitionRepository={ competitionRepository } />
+  return <Competition competitionPresenter={ competitionPresenter } />
 }
 
 export default CompetitionPage;

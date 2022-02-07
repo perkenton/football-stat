9cf111f935e6800f849ebd84f74f53b136642b4e
Alexander Vasiliev <selado@ya.ru>
2022-02-07 17:58:58 +0500

    Добавлена форма и функции поиска, добавлены стили, в useEffect добавлена зависимость от teamId

--- src/components/Team/components/Team.tsx
+++ src/components/Team/components/Team.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Button, DatePicker, Form, Tooltip } from 'antd';
+import { RedoOutlined } from '@ant-design/icons';
 import styles from './Team.module.scss';
 import { TeamPresenter } from '../model/TeamPresenter';
-import { TeamType, Match, ActiveCompetitions } from '../../../common/model';
+import { TeamType, Match, ActiveCompetitions, DatesFilter } from '../../../common/model';
 import MatchesTable from '../../../common/components/MatchesTable/MatchesTable';
 
 
 function Team(props: { teamPresenter: TeamPresenter}) {
+  const DATE_FORMAT = 'DD.MM.YYYY';
+  const [ form ] = Form.useForm();
   const [ team, setTeam ] = useState<TeamType>();
   const [ matches, setMatches ] = useState<Match[]>();
 
@@ -13,14 +17,25 @@ function Team(props: { teamPresenter: TeamPresenter}) {
     setTeam(await props.teamPresenter.getTeam())
   }
 
-  async function getMatches() {
-    setMatches(await props.teamPresenter.getTeamsMatch());
+  async function getMatches(values?: DatesFilter) {
+    setMatches(await props.teamPresenter.getTeamsMatch(values));
   }
 
   useEffect(() => {
     getTeam();
     getMatches();
-  }, [])
+  }, [props.teamPresenter.teamId])
+
+  async function searchMatches(values: DatesFilter) {
+    await props.teamPresenter.historyPush(values);
+    getMatches(values);
+    form.resetFields();
+  }
+
+  async function resetFilter() {
+    await props.teamPresenter.resetFilter();
+    getMatches();
+  }
 
 
   return (
@@ -49,6 +64,50 @@ function Team(props: { teamPresenter: TeamPresenter}) {
         </ul>
         {/* TODO: добавить состав, может в модалке */}
       </div>
+
+      <div className={ styles.formBlock }>
+        <span className={ styles.text }>Фильтр по дате:</span>
+        <Form
+          onFinish={ searchMatches }
+          className={ styles.form }
+          form={ form }
+        >
+          <Form.Item name='dateFrom' rules={[{ required: true, message: 'Выберите дату' }]}>
+            <DatePicker format={ DATE_FORMAT } placeholder='с такого-то' size='small' />
+          </Form.Item>
+          <Form.Item name='dateTo' rules={[{ required: true, message: 'Выберите дату' }]}>
+            <DatePicker format={ DATE_FORMAT } placeholder='по такое-то' size='small' />
+          </Form.Item>
+          <Button
+            htmlType='submit'
+            size='small'
+            className={ styles.button }
+            disabled={ props.teamPresenter.loading }
+          >
+            Найти
+          </Button>
+        </Form>
+
+        <Button
+          size='small'
+          className={ styles.button }
+          onClick={ resetFilter }
+        >
+          Сбросить
+        </Button>
+
+        <Tooltip key={ 2 } title='Обновить список' className={ styles.updateButton }>
+          <Button
+            shape='circle'
+            type='ghost'
+            size='small'
+            icon={ <RedoOutlined /> }
+            onClick={ () => getMatches() }
+            loading={ props.teamPresenter.loading }
+          />
+        </Tooltip>
+      </div>
+
       <MatchesTable matches={ matches } activeTeam={ team?.id } loading={ props.teamPresenter.loading } />
     </>
   )
